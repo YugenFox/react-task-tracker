512201fa45005106d924fe1c9e3df58a85a78408
YugenFox <kalebsday@gmail.com>
2023-01-20 15:32:00 -0600

    Routing added
    In header have conditional rendering to not show the "Add" button when not on the '/' page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import About from "./components/About";
 /*
 npm run server - in one terminal
   - will run the db.json local host 5000
@@ -92,19 +95,37 @@ function App() {
   };
 
   return (
-    <div className="container">
-      <Header
-        onAdd={() => setShowAddTask(!showAddTask)}
-        showAdd={showAddTask}
-      />
+    <Router>
+      <div className="container">
+        <Header
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
 
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-      ) : (
-        "No tasks left"
-      )}
-    </div>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  "No tasks left"
+                )}
+              </>
+            }
+          />
+          <Route path='/about' element={<About />}/>
+        </Routes>
+        
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,23 @@
 import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
 import Button from "./Button";
 
 const Header = ({ title, onAdd, showAdd }) => {
-  const onClick = () => {
-    console.log("Click");
-  };
+  // const onClick = () => {
+  //   console.log("Click");
+  // };
+  const location = useLocation();
 
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color={showAdd ? "red" : "green"} text={showAdd ? "Hide Add Task" : "Show Add Task"} onClick={onAdd} />
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Hide Add Task" : "Show Add Task"}
+          onClick={onAdd}
+        />
+      )}
     </header>
   );
 };
